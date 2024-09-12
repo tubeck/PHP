55121dd00e81a181c2201f72c77f40d3e97af347
tubeck <arthur.b.ribeiro@aluno.senai.br>
2024-09-12 11:46:50 -0300

    Add files via upload

---
+++ LOGIN/default.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>  <!--Para gerar esse início, basta digitar "html:5" e ENTER-->
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            echo "Primeiro PHP!";
+            $nome = "Gabriel Kreischer";
+            echo $nome
+        ?>    
+    </body>
+</html>
